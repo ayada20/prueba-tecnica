e454d03d9cf9b0ea88d1fb2fa944e3408c84552d
ayada20 <edaayada@gmail.com>
2025-10-23 09:56:36 +0200

    feat(indexpage):update index page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,29 @@
-<script></script>
+<script setup lang="ts">
+
+
+import content from "@/assets/content.json" with { type: "json" };
+import '@/assets/estilos.scss';
+const headerItems = content.headerItems;
+const hero = content.hero;
+const experience = content.experience;
+const projects = content.projects;
+const skills = content.skills;
+const education = content.education;
+const footer = content.footer;
+</script>
+
 <template>
-  <div>
-    <h1>hola</h1>
-  </div>
+  <HeaderComponent :headerItems="headerItems" />
+  <main>
+    <HeroComponent :hero="hero" />
+
+    <ExperienceComponent :experience="experience" />
+
+    <ProjectsComponent :projects="projects" />
+
+    <SkillsComponent :skills="skills" />
+
+    <EducationComponent :education="education" />
+  </main>
+  <FooterComponent :footer="footer" />
 </template>
-<style></style>
